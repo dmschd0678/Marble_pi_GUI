86bef7abc0277e8df8d3b9269294449665da4ad0
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-10-06 20:44:11 +0900

    사람 수 입력 받기, 플레이어 표시

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Marble_pi_GUI
  
+### DGSW 2학년 나르샤 프로젝트
--- game_screen.py
+++ game_screen.py
@@ -9,6 +9,23 @@ class game():
         self._root.state('zoomed')
         self._playerNum = int(playerNum.replace("명",""))
 
+        leftFrame = Frame(self._root)
+        leftFrame.pack(side = 'left',fill = 'both',expand = True)
+        rightFrame = Frame(self._root)
+        rightFrame.pack(side = 'right', fill = 'both',expand = True)
+
+        player1 = Label(leftFrame, text = "player1", bg = '#FF7F50')
+        player1.pack(fill='both', expand=True, side='top')
+
+        player2 = Label(rightFrame, text="player2", bg = '#00FFFF')
+        player2.pack(fill='both', expand=True, side='top')
+
+        player3 = Label(leftFrame, text="player3", bg = '#FFD700')
+        player3.pack(fill='both', expand=True, side='bottom')
+
+        player4 = Label(rightFrame, text="player4", bg = '#ADFF2F')
+        player4.pack(fill='both', expand=True, side='bottom')
+
 class player():
     def __init__(self):
         self._money = 1000
