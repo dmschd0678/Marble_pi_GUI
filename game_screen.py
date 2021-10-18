50120756102989ec6a402bc77716d5e65915ad04
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-10-18 19:01:20 +0900

    클래스 구현, 정보 수정 기능

--- game_screen.py
+++ game_screen.py
@@ -2,12 +2,25 @@ from tkinter import *
 import tkinter.font as tkfont
 import requests
 
+url = {"init" : "15.165.88.215:8080/init",
+       "toll" : "15.165.88.215:8080/player/toll?userID=1&cityID=2&usingShield=0",
+       "buy"  : "15.165.88.215:8080/area/buy?userID=1&cityID=2",
+       "key"  : "15.165.88.215:8080/key",
+       "upgrade" : "15.165.88.215:8080/area/upgrade?user_id=1"}
+
 
 class Player():
     def __init__(self):
-        self.money = 1000
-        self.goldenKey = []
+        self.money = 500000
+        self.goldenKey = ["a","b","c","d"]
         self.total_assets = self.money
+
+    def toll(self):
+        pass
+
+    def buy(self):
+        pass
+
 
 class PlayerInfo():
 
@@ -20,47 +33,44 @@ class PlayerInfo():
         self.playerName = Label(frame, text = name, bg = color, font = font)
         self.playerName.pack(fill='x', side='top',anchor = "n")
 
-        self.__moneyInfo = Label(frame, text = "보유 돈 : " +  str(self.player.money), bg = color, font = font)
+        self.__moneyInfo = Label(frame, text = "보유 돈 : " + self.strMoney(self.player.money), bg = color, font = font)
         self.__moneyInfo.pack(fill = 'x', side = 'top',anchor = "n")
 
-        self.__totalAssetsInfo = Label(frame, text = "총 자산 : " + str(self.player.total_assets), bg = color, font = font)
+        self.__totalAssetsInfo = Label(frame, text = "총 자산 : " + self.strMoney(self.player.total_assets), bg = color, font = font)
         self.__totalAssetsInfo.pack(fill = 'x', side = 'top', anchor = 'n')
 
-        self.__goldenKeyInfo = Label(frame, text = "보유 황금열쇠 : " + str([i for i in self.player.goldenKey]), bg = color, font = font)
+        self.__goldenKeyInfo = Label(frame, text = "보유 황금열쇠 : " + str(list(i for i in self.player.goldenKey)), bg = color, font = font)
         self.__goldenKeyInfo.pack(fill = 'x', side = 'top',anchor = "n")
 
-    @property
-    def moneyInfo(self):
-        return self.__moneyInfo
+    def strMoney(self, money):
+        money = str(money // 10000) + "만" + \
+                   (str(money % 10000) + "원"
+                    if str(money % 10000) != "0" else "원")
+        return money
 
-    @moneyInfo.setter
-    def moneyInfo(self, money):
-        self.__moneyInfo.set(money)
+    def setMoneyInfo(self):
+        pass
 
-    @property
-    def totalAssetsInfo(self):
-        return self.__totalAssetsInfo
+    def setTotalAssetsInfo(self):
+        pass
 
-    @totalAssetsInfo.setter
-    def totalAssetsInfo(self,value):
-        self.__totalAssetsInfo.set("value")
+    def setGoldenKeyInfo(self):
+        pass
 
-
-class GameState():
-
+class GameInit():
     def __init__(self,playerNum):
 
-        self._root = Tk()
-        self._root.resizable(False,False)
-        self._root.state('zoomed')
+        root = Tk()
+        root.resizable(False,False)
+        root.state('zoomed')
 
-        self._playerNum = playerNum
+        playerNum = playerNum
 
-        left_Frame = Frame(self._root)  # 왼쪽 프레임
+        left_Frame = Frame(root)  # 왼쪽 프레임
         # left_Frame.pack(side = "left", expand = True,fill = 'both')
         left_Frame.pack(side = "left",fill = 'both')
 
-        right_frame = Frame(self._root) # 오른쪽 프레임
+        right_frame = Frame(root) # 오른쪽 프레임
         # right_frame.pack(side = 'right', expand = True,fill = 'both')
         right_frame.pack(side = 'right',fill = 'both')
 
@@ -80,16 +90,19 @@ class GameState():
         # r_bottom_frame.pack(side = 'bottom', expand = True,fill = 'both')
         r_bottom_frame.pack(side = 'bottom',fill = 'both')
 
-        if self._playerNum >= 2:
+        if playerNum >= 2:
             self.player1 = PlayerInfo(l_top_frame, "player1", '#FF7F50')
             self.player2 = PlayerInfo(r_top_frame, "player2", '#00FFFF')
 
-            if self._playerNum >= 3:
+            if playerNum >= 3:
                 self.player3 = PlayerInfo(l_bototm_frame, "player3", '#FFD700')
 
-                if self._playerNum >= 4:
+                if playerNum >= 4:
                     self.player4 = PlayerInfo(r_bottom_frame, "player4",'#ADFF2F')
 
 
 def start(playerNum):
-    GameState(playerNum)
+    # 초기화
+    # url = "15.165.88.215:8080/init"
+    # res = requests.get(url)
+    GameInit(playerNum)
