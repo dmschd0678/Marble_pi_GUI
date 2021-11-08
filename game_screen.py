bc27b6311cef1953d15c22d215738993d220fd41
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-11-08 19:23:33 +0900

    오류 수정

--- game_screen.py
+++ game_screen.py
@@ -308,7 +308,7 @@ def gamePlay(screen):
             # 주사위 값 받아오기
             diceNum = int(ser.readline().decode("utf-8"))
             print(diceNum)
-            curLocation = landLocation[requests.get(url[playerNum]).json()["user"]["location"]]
+            curLocation = landLocation[requests.get(url["playerInfo"]).json()["user"]["location"]]
             # 서버 주사위 값 넘기기
             requests.patch(url["move"].format(playerNum,diceNum))
             location = requests.get(url["playerInfo"].format(playerNum))
