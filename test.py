4cb5e136df37106ff7667768b169fab7c6aa3ee7
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-11-08 20:09:10 +0900

    오류 수정

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
-req = int(requests.get("http://15.165.88.215:8888/player/0").json()["user"]["location"])
+req = int(requests.get("http://15.165.88.215:8888/area/buy/11/cost?villa=1&building=0&hotel=0").json()["cost"])
 
 print(req)
