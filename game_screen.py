60c1b0f9f75be0545ee595f7dff9bdb8e9ae3c3a
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-11-09 09:28:44 +0900

    플레이어 클래스 수정

--- game_screen.py
+++ game_screen.py
@@ -319,7 +319,7 @@ def gamePlay(screen):
             location = requests.get(url["playerInfo"].format(playerNum))
             location = landLocation[location.json()["user"]["location"]]
 
-            ser.write((f"M {landLocation[curLocation]} {landLocation[location]}").encode("utf-8"))
+            ser.write((f"M {landLocation[curLocation]} {landLocation[location]} {playerNum}").encode("utf-8"))
             print(f"M {landLocation[curLocation]} {landLocation[location]}")
             # ser.write(binascii.unhexlify(f"{landLocation[location]}"))
 
